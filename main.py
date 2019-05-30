6d16c9b31b9b70e40cfb3743252b893cbfec3e2d
Benoit Pimpaud <pimpaudben@gmail.com>
2019-05-30 14:28:40 +0200

    [cleaning] clean main and orchestrator

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from helpers import *
 
 if __name__ == "__main__":
     print("Let's go")
-    orchestrator = Orchestrator()
     x_min = 0
     x_max = 1000
     n = 10000
-    cars = orchestrator.simulation(x_min, x_max, n, 800, leader_function_1, 500)
-    
-    temps = creation_temps(x_min, x_max, n)
-    creation_positions_graphique([car.position for car in cars], temps)
+    position_init = 800
+    leader_function = leader_function_1
+    nb_voitures = 500
+
+    orchestrator = Orchestrator(x_min, x_max, n)
+    cars = orchestrator.simulation(position_init, leader_function_1, nb_voitures)
+    creation_positions_graphique([car.position for car in cars], orchestrator.temps)
